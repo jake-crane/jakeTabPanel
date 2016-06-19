6c756b425dacd4ddc2a22740bc63e2d6f8b8ffd9
Jake Crane <jake@jakecrane.com>
2016-06-19 15:53:03 -0500

    Initial Commit

---
+++ js/jakeTabPanel.js
@@ -0,0 +1,36 @@
+(function () {
+    $.widget("jake.tabPanel", {
+        options: {},
+        _create: function () {
+            this.$tabPanels = this.element.find('[tabHeader]');
+            this.element.empty();
+            this.$ul = $('<ul></ul>');
+            for (var i = 0; i < this.$tabPanels.length; i++) {
+                var tab = $('<li><a href="#">' + this.$tabPanels.eq(i).attr('tabHeader') + '</a></li>');
+                this.$ul.append(tab);
+            }
+            this.$lis = this.$ul.children();
+            this.$lis.eq(0).addClass('active');
+            this.$tabPanels.hide();
+            this.$tabPanels.eq(0).show();
+            this.element.append(this.$ul);
+            this.element.append(this.$tabPanels);
+            this._bindUIActions();
+        },
+        _destroy: function () {
+            this.$tabPanels.show();
+            this.element.empty();
+            this.element.append(this.$tabPanels);
+        },
+        _bindUIActions: function () {
+            var self = this;
+            this.$lis.on('click', function () {
+                self.$lis.removeClass('active');
+                self.$tabPanels.hide();
+                var index = $this.index();
+                self.$tabPanels.eq(index).show();
+                $(this).addClass('active');
+            });
+        }
+    });
+})(jQuery);
